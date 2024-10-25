25858bf2243cb0df06e6bc8a62a7247eee96003b
Svetlana Pavlova <svetlana.pavlova@skol.tech>
2024-10-25 07:01:32 +0300

    better preprocessing and postprocessing

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,21 +1,26 @@
 import os
 import argparse
 
-def postprocess_text(input_dir, output_dir):
+def postprocess_text(raw_dir, input_dir, output_dir):
     os.makedirs(output_dir, exist_ok=True)
     
+    delim = '-='*20
+
     for file_name in os.listdir(input_dir):
         with open(os.path.join(input_dir, file_name), 'r', encoding='utf-8') as f:
+            with open(os.path.join(raw_dir, file_name), 'r') as fraw:
+                init_text = fraw.read() 
             text = f.read()
             # Add custom postprocessing like formatting
             formatted_text = text.capitalize()  # Example of formatting
             with open(os.path.join(output_dir, file_name), 'w', encoding='utf-8') as out_f:
-                out_f.write(formatted_text)
+                out_f.write(init_text + '\n' + delim + '\n' + formatted_text + '\n')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Postprocess translated text")
+    parser.add_argument('--raw', required=True, help="Raw directory with initial text files")
     parser.add_argument('--input', required=True, help="Input directory with translated text files")
     parser.add_argument('--output', required=True, help="Output directory for postprocessed text files")
     args = parser.parse_args()
     
-    postprocess_text(args.input, args.output)
+    postprocess_text(args.raw, args.input, args.output)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -11,6 +11,18 @@ def preprocess_text(input_dir, output_dir):
             with open(os.path.join(output_dir, file_name), 'w', encoding='utf-8') as out_f:
                 out_f.write(clean_text)
 
+def preprocess_text(input_dir, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+    
+    for file_name in os.listdir(input_dir):
+        if not file_name.endswith(".txt"):
+            print("Bad file")
+            continue
+        fn = os.path.join(input_dir, file_name)
+        out_fn = os.path.join(output_dir, file_name)
+        os.system(f"src/cpp_utils/validate '{fn}' > {out_fn}")
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Preprocess English text")
     parser.add_argument('--input', required=True, help="Input directory with raw text files")
@@ -18,3 +30,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     preprocess_text(args.input, args.output)
+
